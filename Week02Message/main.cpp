ea80213effdb688bb74d0546c0b3ebe5d7e9da16
suhye0n <contact.suhyeon@gmail.com>
2022-10-09 19:46:53 +0900

    2022.10.09 상속 및 재정의

--- Week02Message/main.cpp
+++ Week02Message/main.cpp
@@ -1,4 +1,16 @@
 #define _WIN32_WINNT	_WIN32_WINNT_MAXVER
 #include <afxwin.h>
 
-CWinApp app;
+class CMyApp : public CWinApp {
+public:
+	virtual BOOL InitInstance();
+};
+
+BOOL CMyApp::InitInstance()
+{
+	AfxMessageBox(L"파생 클래스의 InitInstance() Overriding");
+
+	return TRUE;
+}
+
+CMyApp app;
